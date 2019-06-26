44ffecfbd290789090a7dabedcc6fbcc25021072
Gustavo Iotti <gustavo.iotti@hotmail.com>
2019-06-25 22:31:15 -0300

    rest

---
+++ back-end/src/main/java/br/edu/utfpr/ProjectExampleApplication.java
@@ -0,0 +1,32 @@
+package br.edu.utfpr;
+import br.edu.utfpr.dao.ClienteDAO;
+import br.edu.utfpr.dao.PaisDAO;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectExampleApplication implements CommandLineRunner {
+
+    private final PaisDAO paisDAO;
+    private final ClienteDAO clienteDAO;
+
+    @Autowired
+    public ProjectExampleApplication(ClienteDAO clienteDAO, PaisDAO paisDAO) {
+        this.paisDAO = paisDAO;
+        this.clienteDAO= clienteDAO;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectExampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+                
+    }
+
+}
---
+++ back-end/src/main/java/br/edu/utfpr/servico/ServicoCliente.java
@@ -0,0 +1,93 @@
+package br.edu.utfpr.servico;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.utfpr.dto.ClienteDTO;
+import br.edu.utfpr.dto.PaisDTO;
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
+@RestController
+public class ServicoCliente {
+
+    private List<ClienteDTO> clientes;
+    private List<PaisDTO> paises;
+
+    public ServicoCliente() {
+        clientes = Stream
+                .of(ClienteDTO.builder().id(1).nome("João").idade(19).telefone("123").limiteCredito(1000).pais(paises.get(3)).build(),
+                        ClienteDTO.builder().id(2).nome("Maria").idade(17).telefone("321").limiteCredito(100).pais(paises.get(2)).build(),
+                        ClienteDTO.builder().id(3).nome("Pedro").idade(20).telefone("999").limiteCredito(2000).pais(paises.get(1)).build())
+                .collect(Collectors.toList());
+        paises = Stream
+                .of(PaisDTO.builder().id(1).nome("Iceland").sigla("IC").codigoTelefone(33).build(),
+                        PaisDTO.builder().id(2).nome("Italia").sigla("IT").codigoTelefone(22).build(),
+                        PaisDTO.builder().id(3).nome("Brasil").sigla("BR").codigoTelefone(55).build()
+                )
+                .collect(Collectors.toList());
+        
+    }
+
+    @GetMapping("/servico/cliente")
+    public ResponseEntity<List<ClienteDTO>> listar() {
+        // public List<ClienteDTO> listar() {
+        // return clientes;
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/servico/cliente/{id}")
+    public ResponseEntity<ClienteDTO> listarPorId(@PathVariable int id) {
+        Optional<ClienteDTO> clienteEncontrado = clientes.stream().filter(p -> p.getId() == id).findAny();
+
+        return ResponseEntity.of(clienteEncontrado);
+    }
+
+    @PostMapping("/servico/cliente")
+    public ResponseEntity<ClienteDTO> criar(@RequestBody ClienteDTO cliente) {
+
+        cliente.setId(clientes.size() + 1);
+        clientes.add(cliente);
+
+        return ResponseEntity.status(201).body(cliente);
+    }
+
+    @DeleteMapping("/servico/cliente/{id}")
+    public ResponseEntity excluir(@PathVariable int id) {
+
+        if (clientes.removeIf(cliente -> cliente.getId() == id))
+            return ResponseEntity.noContent().build();
+
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/servico/cliente/{id}")
+    public ResponseEntity<ClienteDTO> alterar(@PathVariable int id, @RequestBody ClienteDTO cliente) {
+        Optional<ClienteDTO> clienteExistente = clientes.stream().filter(p -> p.getId() == id).findAny();
+
+        clienteExistente.ifPresent(c -> {
+            try {
+                c.setNome(cliente.getNome());
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            c.setIdade(cliente.getIdade());
+            c.setTelefone(cliente.getTelefone());
+            c.setLimiteCredito(cliente.getLimiteCredito());
+        });
+
+        return ResponseEntity.of(clienteExistente);
+    }
+}
